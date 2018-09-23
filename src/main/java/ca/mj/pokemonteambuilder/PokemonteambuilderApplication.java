0ad781cde8fcd56d2c243cb518dec83c29c4b81a
________ <________@192.168.0.20>
2018-09-23 12:27:57 -0400

    First commit

---
+++ src/main/java/ca/mj/pokemonteambuilder/PokemonteambuilderApplication.java
@@ -0,0 +1,12 @@
+package ca.mj.pokemonteambuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonteambuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonteambuilderApplication.class, args);
+	}
+}
